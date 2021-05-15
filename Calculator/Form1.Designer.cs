d0670397eb2a6f032f07e1b14c8c625ccf69279e
Naotarouu <darrencorpuz957@gmail.com>
2021-05-15 15:28:45 +0800

    added minusButton

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace Calculator
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,11 +42,21 @@ namespace Calculator
             this.button1.Text = "plusButton";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "minusButton";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@ namespace Calculator
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
